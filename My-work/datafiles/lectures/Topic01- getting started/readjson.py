3f88a9553d335111a18670cf7acd3cf46100e216
Andre Queiroz Machado <de.machado90@gmail.com>
2024-09-30 09:53:19 +0100

    not graded taks

---
+++ My-work/datafiles/lectures/Topic01- getting started/readjson.py
@@ -0,0 +1,12 @@
+import json
+
+data = {
+    'name': 'Joe',
+    'age': 21,
+    "student": True
+}
+
+file = open("simple.json", 'w') #open a file to writing
+json.dump(data,file,indent=4)
+jsonString = json.dumps(data)
+print(jsonString)
---
+++ My-work/datafiles/lectures/Topic01- getting started/readjson2.py
@@ -0,0 +1,9 @@
+import json
+
+DATADIR = "C:/Users/demac/OneDrive/Desktop/pands/programming-for-data-analytics/My-work/datafiles/"
+FILENAME = "data.json"
+
+with open(DATADIR + FILENAME, "rt") as fp:
+    json_object = json.load(fp)
+for employee in json_object['employees']:
+    print(employee)
