c10ae3336f19499b7d345b96deb0c8273d09af47
Andre Queiroz Machado <de.machado90@gmail.com>
2024-10-01 18:43:04 +0100

    not graded tasks

---
+++ PFDA -assignments/Topic01-representingData/readfromwebjson.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+response = requests.get(url)
+data = response.json()
+print(data)
+
+print(data['bpi']['EUR']['rate_float'])
+
+price_euro = data['bpi']['EUR']['rate_float']
+
+formatted_price = f"{price_euro:,.4f}" # formatting price to euros
+
+print ("Bitcoin price in Euros: ", formatted_price)
