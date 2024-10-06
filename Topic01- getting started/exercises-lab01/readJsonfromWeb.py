92f514bdbf142dbae1b7a4ca137ac3b977cdde7b
Andre Queiroz <de.machado90@gmail.com>
2024-10-06 21:44:21 +0100

    Adding again topic 1

---
+++ Topic01- getting started/exercises-lab01/readJsonfromWeb.py
@@ -0,0 +1,10 @@
+# to read from web
+# Author: Andre
+
+import requests
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+response = requests.get(url)
+data = response.json()
+print(data['bpi']['EUR']['rate_float'])
